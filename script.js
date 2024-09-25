6d6a3a87aba27db72ee0dae6348bb4f6ea406387
LouMek <lmekhelef@gmail.com>
2024-09-25 21:36:30 +0200

    Modifications des changements entre le temps & la pause, avec des indications via un panel et la couleur du background

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
 let lancer = document.getElementById('lancer'); //Variable bouton lancer
 
 let travailMinutes = document.getElementById('minutes'); //Balise p contenant les minutes
-let travailSecondes = document.getElementById('secondes') //Balise p contenant les secondes
+let travailSecondes = document.getElementById('secondes'); //Balise p contenant les secondes
+
+let travailEnCours = document.getElementById('travail'); //On récupère l'élément d'ID 'travail' 
+let pauseEnCours = document.getElementById('pause'); //On récupère l'élément d'ID 'pause' 
 
 let timerActif = false; //Afin de savoir si le timer est actif
 let timerPauseDepart = false;
+let lancementTimer;
+
 
 //Listener permettant de savoir si le bouton "lancer" est cliqué ou non
 lancer.addEventListener('click', function () {
-    let lancementTimer;
     if (timerActif == false) {
-        lancementTimer = setInterval(timer, 1000);
+        travailEnCours.classList.add('actionActive'); //Permet d'activer le visuel "Travail" dans le panel
+        lancementTimer = setInterval(timer, 1000); //lance la fonction timer avec un interval de 1 seconde
     }
-    else if (lancementTimer) {
-        clearInterval(lancementTimer);
+    else {
+        clearInterval(lancementTimer); //permet d'arreter le timer 
         lancementTimer = null;
         timerActif = false;
-        travailMinutes.innerHTML = "25";
+        travailMinutes.innerHTML = "25"; //remet le timer à 25minutes
         travailSecondes.innerHTML = "00";
     }
-    
+
 })
 
 function timer() {
@@ -31,12 +36,25 @@ function timer() {
     if (timerPauseDepart == true) {
         travailMinutes.innerHTML = "0";
         travailSecondes.innerHTML = "05";
-        timerPauseDepart = false;
     }
 
     if (minutes == 0 && secondes == 1) {
-        travailSecondes.innerHTML = '0' + (secondes - 1);
-        timerPauseDepart = true;
+        if(timerPauseDepart == false){
+            travailSecondes.innerHTML = '0' + (secondes - 1);
+            travailEnCours.classList.remove('actionActive'); //Modifi le panel
+            pauseEnCours.classList.add('actionActive');
+            document.body.classList.add('enPause'); //Change la couleur du Body en vert
+            timerPauseDepart = true;
+        }
+        else{
+            travailEnCours.classList.add('actionActive'); 
+            pauseEnCours.classList.remove('actionActive');
+            document.body.classList.remove('enPause'); 
+            travailMinutes.innerHTML = "25";
+            travailSecondes.innerHTML = "00";
+            timerPauseDepart = false;
+        }
+        
     }
 
     else if (secondes > 0) {
