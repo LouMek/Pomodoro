153a0e8050db500ca35a63b28349e71a01a46eb5
LouMek <lmekhelef@gmail.com>
2024-09-25 20:16:20 +0200

    Ajout de la pause juste aprés la fin du temps de travail

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-let lancer = document.getElementById('lancer')
-console.log(lancer);
+let lancer = document.getElementById('lancer'); //Variable bouton lancer
 
+let travailMinutes = document.getElementById('minutes'); //Balise p contenant les minutes
+let travailSecondes = document.getElementById('secondes') //Balise p contenant les secondes
 
-let travailMinutes = document.getElementById('minutes');
-let travailSecondes = document.getElementById('secondes')
-let timerActif = false;
+let timerActif = false; //Afin de savoir si le timer est actif
+let timerPauseDepart = false;
 
-
+//Listener permettant de savoir si le bouton "lancer" est cliqué ou non
 lancer.addEventListener('click', function () {
+    let lancementTimer;
     if (timerActif == false) {
         lancementTimer = setInterval(timer, 1000);
     }
-    else if(lancementTimer){
+    else if (lancementTimer) {
         clearInterval(lancementTimer);
         lancementTimer = null;
         timerActif = false;
         travailMinutes.innerHTML = "25";
         travailSecondes.innerHTML = "00";
     }
+    
 })
 
 function timer() {
@@ -26,15 +28,22 @@ function timer() {
     let minutes = parseInt(travailMinutes.innerHTML); //On récupère les minutes
     let secondes = parseInt(travailSecondes.innerHTML); //On récupère les secondes
 
-    if (minutes == 0 && secondes == 0) {
-        timerActif = False;
+    if (timerPauseDepart == true) {
+        travailMinutes.innerHTML = "0";
+        travailSecondes.innerHTML = "05";
+        timerPauseDepart = false;
+    }
+
+    if (minutes == 0 && secondes == 1) {
+        travailSecondes.innerHTML = '0' + (secondes - 1);
+        timerPauseDepart = true;
     }
 
     else if (secondes > 0) {
-        if(secondes < 11) {
+        if (secondes < 11) {
             travailSecondes.innerHTML = '0' + (secondes - 1);
         }
-        else{
+        else {
             travailSecondes.innerHTML = secondes - 1;
         }
     }
