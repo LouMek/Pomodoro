ddfb20515a3625d10eb1c12dc0a26e85f2f10db1
LouMek <lmekhelef@gmail.com>
2024-10-01 20:54:34 +0200

    Finalisation de la V2 du pomodoro Timer

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+/*
+@author Louisa Mekhelef
+*/
 let boutonLancer = document.getElementById('lancer'); //Variable bouton boutonLancer
 let boutonConfigurer = document.getElementById('configurer'); //Variable bouton boutonConfigurer
 let boutonQuitterFormulaire = document.getElementById('quitter'); //Variable bouton boutonConfigurer
 let boutonValiderFormulaire = document.getElementById('valider');
 
-let minutesTravail = 25; //Valeurs par défaut
+let minutesTravail = 25; //Valeurs par défaut du timer
 let secondesTravail = 0;
 let minutesPause = 5;
 let secondesPause = 0;
@@ -59,6 +62,8 @@ boutonValiderFormulaire.addEventListener('click', function (event) {
     tempsMinutes.innerHTML = minutesTravail;
     tempsSecondes.innerHTML = secondesTravail;
 
+
+
     document.getElementById('pomodoro').style.display = 'flex';
     document.getElementById('formulaire').style.display = 'none';
 });
@@ -106,11 +111,15 @@ function timer() {
             tempsMinutes.innerHTML = minutesTravail;
             tempsSecondes.innerHTML = secondesTravail;
 
+
             document.body.classList.remove('enPause');
             cercleTimer.classList.remove('enPause');
         }
     }
 
+    /* Lorsque les secondes passant à 0, et que les minutes sont > 0, alors on décrémente de 1 minute 
+    et on repasse les secondes à 59.
+    */
     else if (secondes == 0 && minutes > 0) {
         tempsSecondes.innerHTML = '59';
         if (minutes < 11) {
@@ -119,9 +128,11 @@ function timer() {
         else {
             tempsMinutes.innerHTML = '' + (minutes - 1);
         }
-
     }
 
+    /* Lorsque les secondes sont > 0, on décrémente de une seconde,
+    mais si elles sont < 11, on ajoute un '0' devant, pour un meilleur affichage
+    */
     else if (secondes > 0) {
         if (secondes < 11) {
             tempsSecondes.innerHTML = '0' + (secondes - 1);
@@ -132,6 +143,7 @@ function timer() {
     }
 }
 
+/* La fonction permet de réinitialiser le timer et le style et donc de remettre les valeurs par défaut */
 function resetTimer() {
     clearInterval(lancementTimer); //Permet d'arreter le timer 
 
@@ -139,16 +151,21 @@ function resetTimer() {
     timerActif = false;
 
     etatEnCours.innerHTML = "Le timer n'est pas activé";
+
+    affichageZeroSecondes();
     tempsMinutes.innerHTML = minutesTravail; //remet le timer par défaut (25 si l'utilisateur n'a pas configuré)
     tempsSecondes.innerHTML = secondesTravail;
+
 
     document.body.classList.remove('enPause');
     cercleTimer.classList.remove('enPause');
 
-    boutonLancer.innerHTML = '<i class="fa-solid fa-play"></i>';
+    boutonLancer.innerHTML = '<i class="fa-solid fa-play"></i>'; //Pour changer l'icone du bouton
 }
 
-
+/* La fonction permet de vérifier si les conditions sont bien respectées
+afin d'éviter toutes erreurs ou incohérences (temps négatif, trop haut, trop bas...)
+*/
 function conditionTemps() {
     //Minimum du temps de travail = 5 minutes
     if (minutesTravail < 5) {
@@ -158,54 +175,70 @@ function conditionTemps() {
     //Minimum temps pause = 2.30 minutes
     if (minutesPause <= 2 && secondesPause < 30) {
         minutesPause = 2;
-        minutesTravail = 30;
+        secondesPause = 30;
     }
 
     //Maximum du temps de travail = 120 minutes
     if (minutesTravail > 120) {
         minutesTravail = 120;
+        secondesTravail = 0;
     }
     //Maximum du temps de pause = 60 minutes
     if (minutesPause > 60) {
         minutesPause = 60;
+        secondesPause = 0;
     }
 
 
     //Pour ne pas mettre au dessus de 60 secondes 
-    if(secondesPause > 59) {
+    if (secondesPause > 59) {
         secondesPause = 59;
     }
-    if(secondesTravail > 59) {
+    if (secondesTravail > 59) {
         secondesTravail = 59;
     }
     //Pour ne pas mettre en dessous de 0 secondes
-    if(secondesPause < 0) {
+    if (secondesPause < 0) {
         secondesPause = 0;
     }
-    if(secondesTravail < 0) {
+    if (secondesTravail < 0) {
         secondesTravail = 0;
     }
 
-    if(isNaN(minutesTravail)){
+    //Met une valeur par défaut si jamais l'utilisateur ne rentre rien ou met quelque chose d'autre
+    if (isNaN(minutesTravail)) {
         minutesTravail = 25;
     }
-    if(isNaN(secondesTravail)){
+    if (isNaN(secondesTravail)) {
         secondesTravail = 0;
     }
-    if(isNaN(minutesPause)){
+    if (isNaN(minutesPause)) {
         minutesPause = 5;
     }
-    if(isNaN(secondesPause)){
+    if (isNaN(secondesPause)) {
         secondesPause = 0;
     }
 }
 
-function affichageZeroSecondes(){
+/* La fonction permet de rendre l'affichage des secondes < 10 plus agréable (lors de l'initialisation du timer)
+*/
+function affichageZeroSecondes() {
     //Pour ajouter des 0 devant les secondes lorsqu'elles sont en dessous de 10
-    if(secondesTravail < 10){
-        secondesTravail = '0' + secondesTravail;
+    if (secondesTravail < 10) {
+        secondesTravail = "0" + secondesTravail;
+        //console.log(secondesTravail.innerHTML);
     }
-    if(secondesPause < 10 ){
-        secondesPause = '0' + secondesPause;
+    if (secondesPause < 10) {
+        secondesPause = "0" + secondesPause;
+    }
+
+    if (minutesTravail < 10) {
+        minutesTravail = "0" + minutesTravail;
+        //console.log(secondesTravail.innerHTML);
+    }
+    if (minutesPause < 10) {
+        minutesPause = "0" + minutesPause;
     }
 }
+
+
